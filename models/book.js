32673e6411f053d01e85a1d1f335955410b31103
Sky <anvandp18@gmail.com>
2018-09-10 15:43:07 -0700

    update book.js

--- models/book.js
+++ models/book.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
+const publisherSchema = new mongoose.Schema({
+	name: String,
+	year: Number
+});
+
 const bookScheme = new mongoose.bookScheme({
 	title: String,
-	pages: Number
+	pages: Number,
+	author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+	publisher: publisherSchema
 });
 
 module.exports = mongoose.models('Book', bookScheme);
