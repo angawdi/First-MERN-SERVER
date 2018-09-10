dedd7e1ef46939009726e2acf28cfe59d825d70f
Sky <anvandp18@gmail.com>
2018-09-10 14:16:40 -0700

    just index.js added saying hello

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Require needed modules
+const express = require('express');
+
+// Declare app
+const app = express();
+
+// Middleware etc.
+
+// Define routes
+app.get('/', (req, res) => {
+	res.send('Oh hello, world!');
+});
+
+// Listen on 3000
+app.listen(3000);
