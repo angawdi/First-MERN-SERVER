74b5354ab01f56d8d4f7a7f66db6d59c977fde4f
Sky <anvandp18@gmail.com>
2018-09-10 15:03:14 -0700

    update code

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ app.get('/', (req, res) => {
 	});
 });
 
+// trang web http://www.facetheforce.today/
+app.get('/createuser', (req, res) => {
+	db.User.create({
+		name: 'Star Wars Test',
+		image: 'http://www.facetheforce.today/luke/400',
+		birthyear: Math.floor(Math.random() * 80) + 1950,
+		admin: Math.random() < 0.5 ? true : false
+	})
+	.then(result => {
+		res.send('success');
+	})
+	.catch(err => {
+		console.log(err);
+		res.send('error, check your logs!');
+	});
+});
+
 // Listen on 3000
 app.listen(3000);
--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 
 // Connect to your Mongo database. Database is connection string. 
-mongoose.connect(process.env.MONGOOSE_URL || 'mongodb://localhost:27017/hellowdi20');
+mongoose.connect(process.env.MONGOOSE_URL || 'mongodb://localhost:27017/hellowdi20', 
+	{ userNewUrlParser: true });
 
 // Gather up your models: 
 module.exports.User = require('./user');
