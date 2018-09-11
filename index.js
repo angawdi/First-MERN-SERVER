782e4577e6099f07c9fdf57d081715d7e8619be6
Sky <anvandp18@gmail.com>
2018-09-11 11:16:37 -0700

    create controllers

--- index.js
+++ index.js
@@ -8,34 +8,13 @@ const app = express();
 // Get reference to the models
 const db = require('./models');
 
+// Include controllers
+app.use('/users', require('./controllers/users'));
+
 // Define routes
-app.get('/', (req, res) => {
-	db.User.find()
-	.then(users => {
-		res.send(users);
-	})
-	.catch(err => {
-		console.log(err);
-		res.send(err);
-	});
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/client/build/index.html');
 });
 
-// trang web http://www.facetheforce.today/
-app.get('/createuser', (req, res) => {
-	db.User.create({
-		name: 'Star Wars Test',
-		 image: 'http://www.facetheforce.today/luke/400',
-		birthyear: Math.floor(Math.random() * 80) + 1950,
-		admin: Math.random() < 0.5 ? true : false
-	})
-	.then(result => {
-		res.send('success');
-	})
-	.catch(err => {
-		console.log(err);
-		res.send('error, check your logs!');
-	});
-});
-
-// Listen on 3000
+// listen on 3000
 app.listen(3000);
